d17d9d214a3569e8bcda425109a91fd8dd87b997
CHRTGithub <59073361+chunhuiruotong@users.noreply.github.com>
2020-12-01 17:36:46 +0800

    guess.py amendemnt with random func() on 2020-12-1

--- guess.py
+++ guess.py
@@ -1,17 +1,28 @@
 import random
+import pickle
 
-numCount = 0
-sjList = []
+numCount = 0  #计数器变量初始化
+sjList = []   #列表对象声明
+
+
 while numCount <= 9:
-    sjsh = random.randint(0, 10)
-    sjList.append(sjsh)
-    numCount += 1
-for i in range(len(sjList)):
+    sjsh = random.randint(0, 100)     #randint(a,b)产生的随机数包括上下临界值
+    sjList.append(sjsh)               #列表对象append方法，往列表中增加item
+    numCount += 1                     #计数器变量赋值
+
+for i in range(len(sjList)):          #读出列表对象中的item
     print('第', i+1, '个数：', end='')
     print(sjList[i])
 
+    wjnr = '第' + str(i+1) + '个数=' + str(sjList[i])
+    with open('随机数', 'ab') as f:
+        #pickle.dump(sjList[i], f)
+        pickle.dump(wjnr, f)
 
-
+with open('随机数', 'rb') as f:
+    shch = pickle.load(f)
+    if len(shch) > 0:
+        print(shch, '\n')
 
 """
 number=70
