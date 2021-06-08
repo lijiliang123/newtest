3a8a693fc0acc7e40fc6d9fe6c7224a386b25ac6
lijiliang123 <1651789585@qq.com>
2021-06-08 17:24:11 +0800

    修改了提示内容

--- break.py
+++ break.py
@@ -3,7 +3,8 @@ while True:
     if s == 'quit':
         break
     else:
-        print('您输入的字符数为:', len(s))
+        print('您输入的字符数为:', len(s), end='。  ')
+        print('请继续努力，加油啊！')
 
 print('您踩雷了，游戏结束！')
 
