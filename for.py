838887b490f20e736843e8c588e005817380b1e7
lijiliang123 <1651789585@qq.com>
2021-08-17 17:53:53 +0800

    amend parameter

--- for.py
+++ for.py
@@ -1,8 +1,12 @@
-for i in range(0, 10, 1):  #第1个参数代表起始值，第2个参数代表终止值，第3个参数代表步长，输出结果不包括终止值
+for i in range(0, 10, 3):  #第1个参数代表起始值，第2个参数代表终止值，第3个参数代表步长，输出结果不包括终止值
     print(i, end=' ')
 else:                   # for, while, 循环都可以用else子句
     print("\nThe for loop statement is over.")
 
 
-for i in range(50):
-    print(i, end='/*/')
+for i in range(10):
+    print(i, end='*')
+
+'''
+2021-8-17
+'''
