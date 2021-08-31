21109592b0744cd9cf32853377ba94f796b96d09
lijiliang123 <1651789585@qq.com>
2021-08-31 15:04:51 +0800

    another way to import the pylot package

--- grphic.py
+++ grphic.py
@@ -6,8 +6,11 @@ das = pd.read_csv('Uscore.csv', usecols=['语文', '数学', '英语'])
 print(das)
 # das.set_index(['语文', '数学', '英语', '体育'])
 # print(das['语文']['李四'])
+
+'''
 print(das['语文'][1])     # 引用索引，先列后行
 print(das.loc['0':'1', '语文'])  # 使用loc:只能指定行列索引的名字，先行范围，再列
+'''
 
 das.plot(kind='bar')
 plt.show()
