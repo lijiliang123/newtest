d9322cac29fe12cb1c78a23e58f19c1932d5c7d9
lijiliang123 <1651789585@qq.com>
2021-08-31 17:43:57 +0800

    another way to import the pylot package

---
+++ graphic2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import math
+
+price = pd.read_csv('price.csv')
+idx = price.set_index('年月')   # 横轴上展示年月名称
+print(idx)
+idx.plot(kind='bar', figsize=(15, 8), fontsize=10, colormap="cool")
+plt.show()
+
+
+
