15d059810f693d4a4a949e72abc262fd4b4d3132
lijiliang123 <1651789585@qq.com>
2022-01-21 11:04:28 +0800

    一道题目

--- combine.py
+++ combine.py
@@ -1,3 +1,14 @@
+# range(start, stop, [step]) 用于输出一个数字序列，起始值start, 终止值stop，step为步长值
+# start 默认为0，输出的序列数，不包含终止值stop.
+
+c = []
+for i in range(10):
+    c.append(i)
+    print(i, end='-')
+
+print()
+print(c)
+
 # 题目：有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
 
 def threeNums():
@@ -11,7 +22,7 @@ def threeNums():
                     if num not in nums:
                         nums.append(num)
                         count += 1
-    print(count)
+    print("无重复数字的三位数共有：", count, "个。它们如下：")
     print(sorted(nums))
 
 
