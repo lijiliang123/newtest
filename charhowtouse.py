7c41dac36d7c2c7aab54d1037bdaa3645965075d
lijiliang123 <1651789585@qq.com>
2022-03-31 15:56:05 +0800

    character set process

--- charhowtouse.py
+++ charhowtouse.py
@@ -4,6 +4,9 @@
     It is from a web article.
     Let's begin from March, 31st, 2022.
 """
+'''
+    第一部分：字符串切片操作
+'''
 
 test = "python programming"
 print("The string is: ", test)
@@ -11,8 +14,28 @@ for i in range(len(test)):
     print(test[i], '--', end='')
 print()
 
+# 输出首字母
 first_char = test[:1]
 print("The first character of the string is:", first_char)
+# 输出尾字母
 last_char = test[-1:]
 print("The last character of the string is:", last_char)
+
+# 输出除去首字母的所有字符
+except_char = test[1:]
+print("The except first character of the string is:", except_char)
+# 输出除去尾字母的所有字符
+except_last = test[:-1]
+print("The except last character of the string is:", except_last)
+# 输出首字母和尾字母中间的所有字符
+between_two = test[2:-2]
+print("The between two character of the string is:", between_two)
+
+#跳过一个字符输出
+skip_one = test[0:18:2]  #[start:stop:step]
+print("The skip one character of the string is:", skip_one)
+#反转输出字符串
+reverse_char = test[::-1]
+print("The reverse character of the string is:", reverse_char)
+
 
